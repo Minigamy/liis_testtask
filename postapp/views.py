f6246ef360125e2c58cfbc2a38a7b9e4fe693834
Minigamy <ferdinand-f@mail.ru>
2022-02-21 17:12:41 +0300

    Added permission check

--- postapp/views.py
+++ postapp/views.py
@@ -16,39 +16,54 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    if post.private:
+        if request.user.has_perm('postapp.view_post'):
+            return render(request, 'postapp/post_detail.html', {'post': post})
+        else:
+            return render(request, 'postapp/access_denied.html')
     return render(request, 'postapp/post_detail.html', {'post': post})
 
 
 def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+    if request.user.has_perm('postapp.add_post'):
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm()
+        return render(request, 'postapp/post_edit.html', {'form': form})
     else:
-        form = PostForm()
-    return render(request, 'postapp/post_edit.html', {'form': form})
+        return render(request, 'postapp/access_denied.html')
 
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+    if request.user.has_perm('postapp.change_post') and request.user.id == post.author_id:
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'postapp/post_edit.html', {'form': form})
     else:
-        form = PostForm(instance=post)
-    return render(request, 'postapp/post_edit.html', {'form': form})
+        return render(request, 'postapp/access_denied.html')
 
 
 def post_delete(request, pk):
-    if request.method == "GET":
-        Post.objects.filter(id=pk).delete()
-        return redirect('home')
+    post = Post.objects.get(id=pk)
+    if request.user.has_perm('postapp.change_post') and request.user.id == post.author_id:
+        if request.method == "GET":
+            post.delete()
+            return redirect('home')
+    else:
+        return render(request, 'postapp/access_denied.html')
